f90de104fec9aecd4f06ca7f5b43b9e47fbe97be
Andrew Redmond <andrewpredmond@gmail.com>
2015-08-04 14:25:41 -0500

    Boiler

---
+++ metadata.rb
@@ -0,0 +1,8 @@
+name             'aem-dep'
+maintainer       'The Authors'
+maintainer_email 'you@example.com'
+license          'all_rights'
+description      'Installs/Configures aem-dep'
+long_description 'Installs/Configures aem-dep'
+version          '0.1.0'
+
